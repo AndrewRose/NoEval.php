10d8ca4d9ee474cc5e2f08f0970353a03d291cbc
root <root@localhost.localdomain>
2013-05-17 07:17:23 +0100

    Initial real push

---
+++ noeval.php
@@ -0,0 +1,275 @@
+<?php
+
+/*
+ This file is part of NoEval
+ http://github.com/AndrewRose/noeval.php
+ http://andrewrose.co.uk
+ License: GPL; see below
+ Copyright Andrew Rose (hello@andrewrose.co.uk) 2013
+
+    NoEval.php is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    cached.php is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with cached.php.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+class NoEval
+{
+	private $subs = [];
+
+	public function parse($tree, $vars = [])
+	{
+		return $this->evalList($tree, $vars);
+	}
+
+	public function parseVar($var, $vars)
+	{
+		if(is_string($var) && $var{0} == ':')
+		{
+			return $vars[$var];
+		}
+		return $var;
+	}
+
+	public function evalList(&$list, &$vars=[])
+	{
+		if(!is_array($list))
+		{
+			return $list;
+		}
+
+		foreach($list as $tmp => &$a)
+		{
+			if(!is_array($a))
+			{
+				$op = (string)$a;
+				switch($op)
+				{
+					case 'let':
+					{
+						array_shift($list); // remove let op
+						$ax = array_shift($list); // variable name
+
+						$bx = array_shift($list); // expression
+						$vars[$ax] = $this->parseVar($this->evalList($bx, $vars), $vars);
+
+						if($cx = array_shift($list))
+						{
+							$this->evalList($cx, $vars);
+						}
+					}
+					break;
+
+					case 'defun':
+					{
+						array_shift($list); // remove defun op
+						$fncName = array_shift($list);
+						$this->subs[$fncName] = $list;
+						return;
+					}
+					break;
+
+					case 'if':
+					{
+						array_shift($list); // remove if op
+						$cond = array_shift($list);
+						$trueBranch = array_shift($list);
+						$falseBranch = array_shift($list);
+
+						if($this->evalList($cond, $vars))
+						{
+							return $this->evalList($trueBranch, $vars);
+						}
+						else
+						{
+							return $this->evalList($falseBranch, $vars);
+						}
+						return;
+					}
+					break;
+
+					case 'while':
+					{
+						array_shift($list); // drop while op
+						$cond = array_shift($list); // pull the condition expression
+
+						$body = [];
+						while($e = array_shift($list)) // pull remaining s-expression
+						{
+							$body[] = $e;
+						}
+
+						$tmp = $cond;
+						while($this->evalList($tmp, $vars))
+						{
+							$tmp = $cond;
+							$tmp2 = $body;
+							foreach($tmp2 as $e)
+							{
+								$this->evalList($e, $vars);
+							}
+						}
+					}
+					break;
+				}
+			}
+
+			if(is_array($a))
+			{
+				$a = $this->evalList($a, $vars);
+			}
+		}
+
+		$result = 0;
+		$op = (string)array_shift($list);
+
+		switch($op)
+		{
+			case '--':
+			{
+				$ax = array_shift($list);
+				$vars[$ax]--;
+			}
+			break;
+
+			case '++':
+			{
+				$ax = array_shift($list);
+				$vars[$ax]++;
+			}
+			break;
+
+			case '!=':
+			{
+				$result = TRUE;
+				$ax = array_shift($list);
+				$bx = array_shift($list);
+
+				if($this->parseVar($ax, $vars) == $this->parseVar($bx, $vars))
+				{
+					$result = FALSE;
+				}
+			}
+			break;
+
+			case '<':
+			{
+				$result = TRUE;
+				$ax = array_shift($list);
+				$bx = array_shift($list);
+
+				if($this->parseVar($ax, $vars) >= $this->parseVar($bx, $vars))
+				{
+					$result = FALSE;
+				}
+			}
+			break;
+
+			case '>':
+			{
+				$result = TRUE;
+				$ax = array_shift($list);
+				$bx = array_shift($list);
+
+				if($this->parseVar($ax, $vars) <= $this->parseVar($bx, $vars))
+				{
+					$result = FALSE;
+				}
+			}
+			break;
+
+			case '+':
+			{
+				foreach($list as $val)
+				{
+					$result += $this->parseVar($val, $vars);
+				}
+			}
+			break;
+
+			case '-':
+			{
+				$ax = $this->parseVar(array_shift($list), $vars);
+				foreach($list as $val)
+				{
+					$ax -= $this->parseVar($val, $vars);
+				}
+				$result = $ax;
+			}
+			break;
+
+			case '*':
+			{
+				$ax = $this->parseVar(array_shift($list), $vars);
+				foreach($list as $val)
+				{
+					$ax *= $this->parseVar($val, $vars);
+				}
+				$result = $ax;
+			}
+			break;
+
+			case '%':
+			{
+				$ax = $this->parseVar(array_shift($list), $vars);
+				foreach($list as $val)
+				{
+					$ax %= $val;
+				}
+				$result = $ax;
+			}
+			break;
+
+			case '=':
+			{
+				$result = TRUE;
+				$ax = $this->parseVar(array_pop($list), $vars);
+				foreach($list as $val)
+				{
+					if($this->parseVar($val, $vars) != $ax)
+					{
+						$result = FALSE;
+					}
+				}
+			}
+			break;
+
+			case 'echo':
+			{
+				echo "echo>> ".$this->parseVar(array_shift($list), $vars)."\n";
+			}
+			break;
+
+			default:
+			{
+				if(isset($this->subs[$op]))
+				{
+					$vals = [];
+					foreach($list as $idx => $val)
+					{
+						$vals[':'.$idx] = $this->parseVar($val, $vars);
+					}
+
+					$tmp = $this->subs[$op];
+					$result = $this->evalList($tmp, $vals);
+				}
+				else
+				{
+//					exit('Unknown op: '.$op."\n");
+				}
+			}
+			break;
+		}
+
+		return $result;
+
+	}
+}
---
+++ tests.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+ This file is part of NoEval
+ http://github.com/AndrewRose/noeval.php
+ http://andrewrose.co.uk
+ License: GPL; see below
+ Copyright Andrew Rose (hello@andrewrose.co.uk) 2013
+
+    NoEval.php is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    cached.php is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with cached.php.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+include_once("noeval.php");
+
+$t = new NoEval;
+
+echo $t->parse(["+", 3, 4]);
+
+// if
+$t->parse(["if", ["=", 0, ["%", 5, 3]], ["echo", "bar"], ["echo", "foo"]]);
+$t->parse(["if", ["=", ["%", 5, 5], 0], ["echo", "bar"], ["echo", "foo"]]);
+$t->parse(["if", [">", 4, 10], ["echo", "greater than!\n"], ["echo", "less than!\n"]]);
+
+// variables
+$t->parse(["let", ":pi", 3.14159265359, ["echo", ["*", ":pi", 10, 10]]]);
+
+// functions
+$t->parse(["defun", "add", "+", ["*", ":0", ":1"], ":2"]);
+$t->parse(["echo", ["add", 5, 2, 3]]);
+$t->parse(["echo", ["add", 2, ["add", 5, ["-", 10, 5], 3], 32]]);
+$t->parse(["echo", ["add", 98, ["add", 54, ["-", 124, 65], 2], 67]]);
+
+//recursion
+$t->parse(["defun", "rec", ["if", ["<", ":0", "10"], [["echo", ":0"], ["++", ":0"], ["rec", ":0"]], ["echo", ":0"]]]);
+$t->parse(["rec", 1]);
+
+//fizzbuzz
+$t->parse(["let", ":i", 1, [["echo", "Fizzbuzz example"],
+	["while", ["<", ":i", ["+", 100, 1]],
+		["if", ["=", ["%", ":i", 3], 0],
+			["if", ["=", ["%", ":i", 5], 0],
+				["echo", "FizzBuzz"],
+				["echo", "Fizz"]
+			],
+			["if", ["=", ["%", ":i", 5], 0],
+				["echo", "Buzz"],
+				["echo", ":i"]
+			],
+		],
+		["++", ":i"]
+	]]
+]);
+
+//fibonacci
+$t->parse(["let", ":z", 0,
+	["while", ["<", ":z", 1000],
+		["let", ":z", ["+", ":x", ":y"]],
+		["echo", ":z"],
+		["let", ":x", ":y"],
+		["let", ":y", ":z"]
+	]
+],[":x" => 1, ":y" => 2]);
+
+/*while(1)
+{
+	$line = readline("NoEval: ");
+	readline_add_history($line);
+	if($line == 'exit') exit();
+	echo $t->parse(json_decode($line), []);
+}*/
+
